514373dd1ec8e9cf06bb25b4c5d0af4412a2662b
aj <ali.becerra1224@gmail.com>
2022-02-26 22:53:30 -0800

    1)turned on termguicolors ... 2) did one dark plugin add & added markdown previewer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,10 +44,14 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
+
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim"
+  -- use "lunarvim/darkplus.nvim"
+  use "joshdick/onedark.vim"
+  use "sickill/vim-monokai"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
